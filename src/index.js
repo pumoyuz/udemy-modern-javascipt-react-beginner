ff4ee97a568698215fda1bd9881cce78c2759134
pumoyuz <50436595+pumoyuz@users.noreply.github.com>
2022-03-16 01:26:24 +0900

    27. 論理演算子の本当の意味を知ろう && ||

--- src/index.js
+++ src/index.js
@@ -198,7 +198,30 @@
 //   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
 // console.log(formattedNum);
 
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲です";
-};
-console.log(checkSum(50, 40));
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲です";
+// };
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrue");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrue");
+// }
+
+// || は左側がfalseなら右側を返す 左がtrueなら左を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す 左がfalseなら左を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
