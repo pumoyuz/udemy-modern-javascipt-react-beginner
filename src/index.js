7d971a44baa8c3dc3d8f349bdec3665d03e19ee3
pumoyuz <50436595+pumoyuz@users.noreply.github.com>
2022-03-24 01:27:07 +0900

    32. タスクの追加機能(テキスト)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liを生成
+  const li = document.createElement("li");
+  li.className = "test";
+  li.innerText = inputText;
+
+  // liの子要素に各要素を設定
+  // li.appendChild(btn);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
